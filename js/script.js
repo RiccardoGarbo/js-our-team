46a40c28c97e8345c8d97e040eb288d0d39f3ffa
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-15 14:25:50 +0100

    create variable html, create variable array object

---
+++ js/script.js
@@ -0,0 +1,59 @@
+//Variabili elementi html
+
+const main = document.querySelector('main')
+
+const infoTeam = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder e CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Carrol',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg'
+    }
+
+]
+//Numero del team
+const numberTeam = 6
+let cardProfile = ' '
+
+for (let i = 0; i < numberTeam; i++) {
+    cardProfile = '<div>'
+
+
+
+
+
+
+
+    cardProfile += '</div>'
+}
+
+
+
+
+
+
+
