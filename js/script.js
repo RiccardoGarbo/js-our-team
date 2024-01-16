a8572c7447979c5058b638e9dfcabecf1c093347
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-16 09:48:48 +0100

    fix img tag

--- js/script.js
+++ js/script.js
@@ -6,32 +6,32 @@ const infoTeam = [
     {
         name: 'Wayne Barnett',
         role: 'Founder e CEO',
-        photo: 'img/wayne-barnett-founder-ceo.jpg'
+        photo: 'wayne-barnett-founder-ceo.jpg'
     },
     {
         name: 'Angela Carrol',
         role: 'Chief Editor',
-        photo: 'img/angela-caroll-chief-editor.jpg'
+        photo: 'angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        photo: 'img/walter-gordon-office-manager.jpg'
+        photo: 'walter-gordon-office-manager.jpg'
     },
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        photo: 'img/angela-lopez-social-media-manager.jpg'
+        photo: 'angela-lopez-social-media-manager.jpg'
     },
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        photo: 'img/scott-estrada-developer.jpg'
+        photo: 'scott-estrada-developer.jpg'
     },
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        photo: 'img/barbara-ramos-graphic-designer.jpg'
+        photo: 'barbara-ramos-graphic-designer.jpg'
     }
 
 ]
@@ -48,12 +48,13 @@ for (let i = 0; i < numberTeam; i++) {
     for (let key in infoTeam[i]) {
         cardProfile = '<div class="card border-0 bg-white">'
         cardProfile +=
-            `<img src="${photo}" class="card-img-top" alt="...">
+            `<img src="img/${photo}" class="card-img-top" alt="...">
                 <div class="card-body text-center">
                     <h5 class="card-title">${name}</h5>
                     <p class="card-text">${role}</p>
             </div>`
         cardProfile += '</div>'
+        console.log(cardProfile)
     }
     //Stampa in pagina
     main.innerHTML += cardProfile
